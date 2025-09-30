d81a20966aec49d7b1cd3cc0c5ff5c6d8b88fb17
BharaniPrasath <bharaniprasath6380@gmail.com>
2025-09-30 22:36:59 +0530

    portpolio

---
+++ index.js
@@ -0,0 +1,23 @@
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 110; // offset for navbar
+        const sectionHeight = section.clientHeight;
+
+        if (pageYOffset >= sectionTop && pageYOffset < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
